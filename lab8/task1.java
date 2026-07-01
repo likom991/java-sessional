1d6bd2a12b970daf7132b933e65e9532f87677ae
AD IL <adilctg890@gmail.com>
2026-07-01 16:39:02 +0600

    Add Animal, Dog, and Cat classes with sound methods
    Implemented inheritance with Animal, Dog, and Cat classes, each providing their own sound method.

---
+++ lab8/task1.java
@@ -0,0 +1,27 @@
+void sound() { 
+System.out.println("Some generic sound"); 
+} class Animal { 
+
+} 
+class Dog extends Animal { 
+@Override 
+void sound() { 
+System.out.println("Dog barks"); 
+} 
+} 
+class Cat extends Animal { 
+@Override 
+void sound() { 
+System.out.println("Cat meows"); 
+} 
+} 
+public class Main { 
+public static void main(String[] args) { 
+Animal a = new Animal(); 
+Dog d = new Dog(); 
+Cat c = new Cat(); 
+a.sound(); 
+d.sound(); 
+c.sound(); 
+} 
+} 
