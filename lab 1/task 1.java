d9f0dde4ca6d8c27e9c03d47d0b8b7102aaafb8e
AD IL <adilctg890@gmail.com>
2026-07-01 16:06:53 +0600

    Add Info class to display student information

---
+++ lab 1/task 1.java
@@ -0,0 +1,19 @@
+public class Info {
+
+    public static void main(String[] args) {
+
+        String name = "AD IL";
+        String studentID = "E253067";
+        int age = 19;
+        String phone = "01877425678";
+        String email = "adilctg890@gmail.com";
+        String address = "patenga, Chittagong";
+
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Age: " + age);
+        System.out.println("Phone: " + phone);
+        System.out.println("Email: " + email);
+        System.out.println("Address: " + address);
+    }
+}
